45ee8a08a48eb8d2e4d557b88351dbaa16bb6b87
Kanchan <kanchan2001pal@gmail.com>
2023-11-28 11:11:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect , useState } from 'react';
+import MovieCard from "./MovieCard";
+
+import "./App.css"
+import { FaSearch } from 'react-icons/fa';
+
+// 1b2f5cd2
+
+const API_URL = "http://www.omdbapi.com?apikey=1b2f5cd2";
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>MoviesHub</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <FaSearch onClick={() => searchMovies(searchTerm) }/>
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
